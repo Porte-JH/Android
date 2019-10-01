01665aa9e8d0be19b3c25fc72a3bcaea1126993c
Porte-JH <50349589+Porte-JH@users.noreply.github.com>
2019-10-01 23:24:56 +0900

    3주차 연습

--- main/java/com/example/myapplication/MainActivity.java
+++ main/java/com/example/myapplication/MainActivity.java
@@ -1,52 +1,104 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.myapplication.Student;
+import com.example.myapplication.DBHelper;
+import static android.text.TextUtils.isEmpty;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
+    private final boolean D = true;
+
+    Student s;
+    DBHelper db;
+
+    private EditText num, name, phone;
+    private Button btn_select, btn_save;
+    private TextView message;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        num = (EditText) findViewById(R.id.num);
+        name = (EditText) findViewById(R.id.name);
+        phone = (EditText) findViewById(R.id.phone);
+        //전화번호 형식으로 입력.
+        phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
+
+        btn_select = (Button) findViewById(R.id.btn_select);
+        btn_save = (Button) findViewById(R.id.btn_save);
+
+        message = (TextView) findViewById(R.id.message);
+
+        if(D){
+            Log.i(TAG, "JUST START!!");
+        }
+        //db에서 사용중인 테이블값 가져오기. * getApplicationContext()
+        db = new DBHelper(getApplicationContext());
+
+        btn_select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                if(!isEmpty(num.getText())){ //num값이 null이 아닐 때
+                    //trim() == 공백 제거
+                    s = db.select(num.getText().toString().trim());
+                    db.closeDB();
+
+                    if(s != null){ // student테이블 s 의 값이 null이 아닐 경우
+                        onSetUI(s);
+                    } else {
+                        onSetNull();
+                    }
+                }
+
+            }
+        });
+
+
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isEmpty(num.getText())){ //num값이 null이 아닐 때
+                    //trim() == 공백 제거
+                    s = db.InsertORUpdate( num.getText().toString().trim(),
+                                            name.getText().toString().trim(),
+                                            phone.getText().toString().trim());
+                    db.closeDB();
+
+                    if(s != null){ // student테이블 s 의 값이 null이 아닐 경우
+                        onSetUI(s);
+                    } else {
+                        onSetNull();
+                    }
+                }
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+    public void onSetUI(Student s){
+        num.setText(s.getNum());
+        name.setText(s.getName());
+        phone.setText(s.getPhone());
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onSetNull(){
+        //num.setText("");
+        name.setText("");
+        phone.setText("");
     }
 }
